8d648b6cd6f71001b17b28dc57466c99c367da97
RmiliYahya1 <rmiliyahya2@gmail.com>
2025-12-13 23:34:39 +0100

    obstacle.js

--- obstacle.js
+++ obstacle.js
@@ -1,13 +1,26 @@
 class Obstacle {
   constructor() {
-    this.w = 100; // Largeur
-    this.h = 100; // Hauteur
-    this.x = random(width- this.w); // Commence à droite
-    this.y = random(height - this.h); // Hauteur aléatoire
-    this.speed = 1; // Vitesse de l'obstacle
+    this.w = 100;
+    this.h = 100;
+    this.x = random(width - this.w);
+    this.y = random(height - this.h);
+    this.speed = 1;
+
     this.image = random(obstacleImages);
-    // position vector (center) and approximate radius for avoidance
     this.pos = createVector(this.x + this.w / 2, this.y + this.h / 2);
+    this.r = max(this.w, this.h) / 2;
+  }
+
+  update() {
+    this.x -= this.speed;
+
+    if (this.x < -this.w) {
+      this.x = width + random(0, 100);
+      this.y = random(height - this.h);
+    }
+
+    this.pos.x = this.x + this.w / 2;
+    this.pos.y = this.y + this.h / 2;
     this.r = max(this.w, this.h) / 2;
   }
 
@@ -15,17 +28,4 @@ class Obstacle {
     image(this.image, this.x, this.y, this.w, this.h);
   }
 
-  update() {
-    this.x -= this.speed; // Déplacement vers la gauche
-
-    // Si l'obstacle sort de l'écran à gauche, on le remet à droite
-    if (this.x < -this.w) {
-      this.x = width;
-      this.y = random(height - this.h);
-    }
-    // keep pos and radius in sync
-    this.pos.x = this.x + this.w / 2;
-    this.pos.y = this.y + this.h / 2;
-    this.r = max(this.w, this.h) / 2;
-  }
 }
