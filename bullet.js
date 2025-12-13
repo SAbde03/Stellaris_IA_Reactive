c665081f5a5b4a8a29085bfbce04e8ba78a0dcff
RmiliYahya1 <rmiliyahya2@gmail.com>
2025-12-14 00:10:19 +0100

    bullet.js

--- bullet.js
+++ bullet.js
@@ -1,61 +1,41 @@
+
+
 class Bullet extends Vehicle {
-
   constructor(x, y, direction) {
-  super(x, y, null);
-  this.vel = direction.copy().setMag(15); // vitesse initiale de la balle
-  this.maxSpeed = 8; // éviter que super.update() ne la ramène à 2
-  this.maxForce = 0.3; // permet de seek un peu
-  // durée de vie en secondes
-  this.life = 6;
-  this.radius = 5;
-}
+    super(x, y, null);
 
-  
+    this.vel = direction.copy().setMag(15);
+    this.maxSpeed = 12;
+    this.maxForce = 0.35;
+
+    this.radius = 5;
+    this.life = 6; // seconds
+    this.pathLength = 20;
+  }
+
+  update() {
+    super.update();
+    this.life -= deltaTime / 1000;
+  }
 
   show() {
-  fill(255, 200, 0);
-  circle(this.pos.x, this.pos.y, this.radius * 2);
-  this.path.forEach((p, index) => {
-  
-      //si vehicle est le premier du tableau (le vaisseau), on dessine en orange
-      stroke('rgb(255, 166, 0)');
-      
-      strokeWeight(2);
-      fill('rgb(255, 123,0)');
-      circle(p.x, p.y, map(index, 0, this.path.length, 0, 10));
-  
-    });
-}
+    noStroke();
+    fill(255, 200, 0);
+    circle(this.pos.x, this.pos.y, this.radius * 2);
 
-  // mise à jour spécifique au missile: mouvement + décrément de vie
-  update() {
-    // mouvement via la logique Vehicle
-    super.update();
-    // décrémenter la vie selon le temps écoulé (ms -> s)
-    if (typeof deltaTime !== 'undefined') {
-      this.life -= deltaTime / 1000;
-    } else {
-      // fallback si deltaTime n'est pas dispo
-      this.life -= 1 / 60;
-    }
-    // 1. Ajouter la position actuelle
-    // IMPORTANT : Utilise .copy(), sinon tous les points du chemin bougeront avec le vaisseau
-    this.path.push(this.pos.copy());
-
-    // 2. Limiter la taille (C'est ici que tu gères la longueur)
-    // Plus le chiffre est petit, plus la traînée est courte
-    if (this.path.length > 20) { 
-        this.path.shift(); // Supprime le premier élément (le plus vieux)
+    noFill();
+    stroke(255, 140, 0, 120);
+    for (let i = 0; i < this.path.length; i++) {
+      const p = this.path[i];
+      circle(p.x, p.y, map(i, 0, this.path.length - 1, 0, 10));
     }
   }
- 
 
   isAlive() {
     return (
-      this.life > 0 &&
-      this.pos.x > 0 && this.pos.x < width &&
-      this.pos.y > 0 && this.pos.y < height
+        this.life > 0 &&
+        this.pos.x > -20 && this.pos.x < width + 20 &&
+        this.pos.y > -20 && this.pos.y < height + 20
     );
   }
-
 }
