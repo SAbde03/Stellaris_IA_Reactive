8c0ae512e8e26a4565d4151bc1c21e921b055898
RmiliYahya1 <rmiliyahya2@gmail.com>
2025-12-13 23:26:27 +0100

    Star.js

--- Star.js
+++ Star.js
@@ -1,22 +1,30 @@
 class Star{
   constructor(){
-    this.x=random(width);
-    this.y=random(height);
-    this.size=random(1,5);
-    this.speed=random(1,1);
+    //this.x=random(width);
+    //this.y=random(height);
+    //this.size=random(1,5);
+    //this.speed=random(1,1);
+    this.reset(true);
+  }
+
+  reset(randomX = false){
+    this.x = randomX? random(width) : width + random(0,50);
+    this.y = random(height);
+    this.size = random(1, 3.5);
+    this.speed = random(0.5, 2.2);
+  }
+
+  update(){
+    this.x -= this.speed;
+    if (this.x < -this.size){
+      this.reset(false);
+    }
   }
   
   show(){
     noStroke();
-    fill;
-    ellipse(this.x,this.y,this.size);
+    fill(255);
+    circle(this.x,this.y,this.size);
   }
-  update(){
-    this.x -= this.speed;
-    
-    if(this.x < -this.size){
-      this.x=width - this.size;
-      this.y=random(height);
-    }
-  }
+
 }
